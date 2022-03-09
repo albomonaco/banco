0da3c3f0a7608b3fcac44b94a7741bc7bc1fb34b
AlboMonco <albomonaco@gmail.com>
2022-03-09 13:02:04 -0300

    Added new functions for login and new features for the bank management

--- main.c
+++ main.c
@@ -3,20 +3,65 @@
 #include "main.h"
 #include "entrada.h"
 
-int bloqueado = 0;
+int logado = 0;
+
+void cumprimentar(){
+    char nome[70];
+    
+    FILE* arquivo_nome = fopen("./db/nome.txt", "r");    
+    fscanf(arquivo_nome, "%s", nome);
+
+    printf("Olá %s!\n", nome);
+
+    fclose(arquivo_nome);
+}
+
+void ver_saldo(){
+
+    FILE* arquivo_saldo = fopen("./db/saldo.txt", "r");
+
+    float saldo;
+    fscanf(arquivo_saldo, "%f", &saldo);
+    
+    printf("\nSaldo: %0.2f reais.\n", saldo);
+    
+    
+    fclose(arquivo_saldo);
+}
+
+int comandar(){
+    
+    int comando;
+
+    printf("\nO que você deseja fazer?\n");
+    printf("(1) Ver Saldo (2) Sair\n\n");
+
+    scanf("%d", &comando);
+
+    return comando;
+}
 
 int main(){
 
     bemvindo();
-    
-    do{
-        entrar();
-        
-        if(validou()){
-            entrada();
+    login(&logado);
+
+    if(logado) cumprimentar();
+
+    while(logado){
+
+        switch(comandar()){
+            case SALDO:
+                ver_saldo();
+                break;
+            case SAIR:
+                logado = 0;
+                
+                break;
+            default:
+                printf("\nOpção inválida.\n");
+                break;
         }
 
-        acesso_negado(bloqueado);
-        
-    } while(!bloqueado);
+    }
 }
